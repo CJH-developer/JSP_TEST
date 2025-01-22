9118995b574a2e17cedc5233a52059e7a0798acb
cjh <flyflow4@gmail.com>
2025-01-22 18:05:14 +0900

    20250122 - JSP Rq 객체

--- src/main/java/com/example/springExample/GuGudanServlet.java
+++ src/main/java/com/example/springExample/GuGudanServlet.java
@@ -19,14 +19,14 @@ public class GuGudanServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        resp.setCharacterEncoding("UTF-8"); 
-        resp.setContentType("text/html; charset=utf-8");
-        
-        int dan = Integer.parseInt(req.getParameter("dan"));
+        Rq rq = new Rq(req, resp);
+
+        int dan = rq.getIntParam("dan", 0);
+        int limit = rq.getIntParam("limit", 0);
+
         resp.getWriter().append(String.format("<h1>구구단 %d단</h1>", dan));
 
-        for(int i=1; i<10; i++){
+        for(int i=1; i<limit; i++){
             resp.getWriter().append(String.format("<div>%d * %d = %d", dan, i, dan * i));
         }
 
---
+++ src/main/java/com/example/springExample/Rq.java
@@ -0,0 +1,42 @@
+package com.example.springExample;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+public class Rq {
+
+    private final HttpServletRequest req;
+    private final HttpServletResponse res;
+
+    public Rq(HttpServletRequest req, HttpServletResponse res) throws UnsupportedEncodingException {
+        this.req = req;
+        this.res = res;
+
+        res.setCharacterEncoding("UTF-8");
+        res.setContentType("text/html");
+        req.setCharacterEncoding("UTF-8");
+    }
+
+    public int getIntParam(String paramName, int defaultValue){
+
+        String value = req.getParameter(paramName);
+
+        if(value == null){
+            return defaultValue;
+        }
+        try {
+            return Integer.parseInt(value);
+        }catch (NumberFormatException e){
+            return defaultValue;
+        }
+    }
+
+    public void appendBody(String str) throws IOException {
+
+        res.getWriter().append(str);
+    }
+
+}
